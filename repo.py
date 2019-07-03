9596de73e40469588c710fba5c584cb824659ca1
gshubh <mg24369@gmail.com>
2019-07-03 13:58:09 +0530

    New Commit

--- repo.py
+++ repo.py
@@ -144,10 +144,10 @@ def main():
     #g = Github("gshubh", "passwd")
     create_new_repository("bucketlist")
     #repo_dir =test
-
+    git_pull('/home/ubuntu-1804/Desktop/bucketlist')
     git_add('/home/ubuntu-1804/Desktop/bucketlist/repo.py', '/home/ubuntu-1804/Desktop/bucketlist')
     git_commit("New Commit", '/home/ubuntu-1804/Desktop/bucketlist')
-    push_branch('/home/ubuntu-1804/Desktop/bucketlist', 'master')
+    #push_branch('/home/ubuntu-1804/Desktop/bucketlist', 'master')
     #push_all_branch('/home/ubuntu-1804/Desktop/bucketlist')
 
 
