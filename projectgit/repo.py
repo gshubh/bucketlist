17a6729830b8fd2f16257ac5f36554eac6deecce
gshubh <skg31297@gmail.com>
2019-07-24 12:13:39 +0530

    New_commit

--- projectgit/repo.py
+++ projectgit/repo.py
@@ -564,185 +564,186 @@ def replymenu ():
     choice = getchoice()
 
     if choice == 1:
-        url = str(input('Enter repository URL : '))
-        to_path = str(input('Enter path to directory where you want to clone : '))
+        url = (raw_input('Enter repository URL : '))
+        to_path = (raw_input('Enter path to directory where you want to clone : '))
         _clone_repo(url, to_path)
-        main_menu()
+        main()
 
     elif choice == 2:
-        username = str(input('Enter your Github username: '))
-        full_name = str(input('Enter the name which you want to give to the repository: '))
+        username = (raw_input('Enter your Github username: '))
+        full_name = (raw_input('Enter the name which you want to give to the repository: '))
         g = _get_username_and_password("gshubh")
         _create_repo(g, full_name)
-        main_menu()
+        main()
 
     elif choice == 3:
-        username = str(input('Enter your Github username: '))
-        full_name = str(input('Enter the name which you want to give to the repository: '))
+        username = (raw_input('Enter your Github username: '))
+        full_name = (raw_input('Enter the name which you want to give to the repository: '))
         g = _get_username_and_password(username)
         _create_repo(g, full_name)
-        main_menu()
+        main()
 
     elif choice == 4:
-        user_name = str(input('Enter your Github username: '))
-        commiter_name = str(input('Enter Commiter name: '))
-        commiter_email = str(input('Enter Commiter email_address: '))
-        cloned_repo_directory = str(input('Enter the path/to/cloned_repo_directory: '))
-        commit_message = str(input('Enter Commit message: '))
+        user_name = (raw_input('Enter your Github username: '))
+        commiter_name = (raw_input('Enter Commiter name: '))
+        commiter_email = (raw_input('Enter Commiter email_address: '))
+        cloned_repo_directory = (raw_input('Enter the path/to/cloned_repo_directory: '))
+        commit_message = (raw_input('Enter Commit message: '))
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
         _commit_to_repo(repo, cloned_repo_directory, commiter_name, commiter_email, commit_message)
         _push_to_repo(user_name, cloned_repo_directory)
-        main_menu()
+        main()
 
     elif choice == 5:
-        user_name = str(input('Enter your Github username: '))
-        cloned_repo_directory = str(input('Enter the path/to/cloned_repo_directory: '))
+        user_name = (raw_input('Enter your Github username: '))
+        cloned_repo_directory = (raw_input('Enter the path/to/cloned_repo_directory: '))
         _push_to_repo(user_name, cloned_repo_directory)
-        main_menu()
+        main()
 
     elif choice == 6:
-        path_to_repo = str(input('Enter the path/to/repository: '))
+        path_to_repo = (raw_input('Enter the path/to/repository: '))
         _get_current_working_branch(path_to_repo)
-        main_menu()
+        main()
 
     elif choice == 7:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
-        name = str(input('Enter the branch name: '))
+        name = (raw_input('Enter the branch name: '))
         _get_branch(repo, name)
-        main_menu()
+        main()
 
     elif choice == 8:
-        path_to_repo = str(input('Enter the path/to/repository: '))
+        path_to_repo = (raw_input('Enter the path/to/repository: '))
         _get_current_working_branch(path_to_repo)
-        main_menu()
+        main()
 
     elif choice == 9:
-        path_to_repo = str(input('Enter the path/to/repository: '))
-        branch_name = str(input('Enter the branch name: '))
+        path_to_repo = (raw_input('Enter the path/to/repository: '))
+        branch_name = (raw_input('Enter the branch name: '))
         _git_checkout(path_to_repo, branch_name)
-        main_menu()
+        main()
 
     elif choice == 10:
-        path_to_repo = str(input('Enter the path/to/repository: '))
-        branch_name = str(input('Enter the branch name: '))
+        path_to_repo = (raw_input('Enter the path/to/repository: '))
+        branch_name = (raw_input('Enter the branch name: '))
         _git_checkout(path_to_repo, branch_name)
-        main_menu()
+        main()
 
     elif choice == 11:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
-        working_branch = str(input('Enter the branch which you want to merge to master: '))
+        working_branch = (raw_input('Enter the branch which you want to merge to master: '))
         _merge_branch_to_master(repo, working_branch)
-        main_menu()
+        main()
 
     elif choice == 12:
-        path_to_repo = str(input('Enter the path/to/repository: '))
-        branch_name = str(input('Enter the branch name which you want to rebase to master: '))
-        commiter_name = str(input('Enter Commiter name: '))
-        commiter_email = str(input('Enter Commiter email_address: '))
-        commit_message = str(input('Enter Commit message: '))
+        path_to_repo = (raw_input('Enter the path/to/repository: '))
+        branch_name = (raw_input('Enter the branch name which you want to rebase to master: '))
+        commiter_name = (raw_input('Enter Commiter name: '))
+        commiter_email = (raw_input('Enter Commiter email_address: '))
+        commit_message = (raw_input('Enter Commit message: '))
         _rebasing(path_to_repo, branch_name, commiter_name, commiter_email, commit_message)
-        main_menu()
+        main()
 
     elif choice == 13:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
-        name = str(input('Enter the branch name: '))
+        name = (raw_input('Enter the branch name: '))
         _delete_branch(repo, name)
-        main_menu()
+        main()
 
     elif choice == 14:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
-        file_path = str(input('Enter the file path inside the project directory: '))
-        commit_message = str(input('Enter Commit message: '))
-        content = str(input('Enter the content which you want to add to the file: '))
-        branch = str(input('Enter the branch name: '))
+        file_path = (raw_input('Enter the file path inside the project directory: '))
+        commit_message = (raw_input('Enter Commit message: '))
+        content = (raw_input('Enter the content which you want to add to the file: '))
+        branch = (raw_input('Enter the branch name: '))
         _create_new_file(repo, file_path, content, commit_message, branch)
-        main_menu()
+        main()
 
     elif choice == 15:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
-        file_path = str(input('Enter the path/to/repository: '))
-        commit_message = str(input('Enter Commit message: '))
-        content = str(input('Enter the content which you want to add to the file: '))
-        branch = str(input('Enter the branch name: '))
+        file_path = (raw_input('Enter the path/to/repository: '))
+        commit_message = (raw_input('Enter Commit message: '))
+        content = (raw_input('Enter the content which you want to add to the file: '))
+        branch = (raw_input('Enter the branch name: '))
         ref = branch
         _update_a_file(repo, file_path, content, commit_message, ref, branch)
+        main()
 
     elif choice == 16:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
-        file_path = str(input('Enter the path/to/repository: '))
-        commit_message = str(input('Enter Commit message: '))
-        branch = str(input('Enter the branch name: '))
+        file_path = (raw_input('Enter the path/to/repository: '))
+        commit_message = (raw_input('Enter Commit message: '))
+        branch = (raw_input('Enter the branch name: '))
         ref = branch
         _delete_a_file(repo, file_path, commit_message, ref, branch)
-        main_menu()
+        main()
 
     elif choice == 17:
-        repo_dir = str(input('Enter the path/to/repository: '))
-        commit_message = str(input('Enter Commit message: '))
+        repo_dir = (raw_input('Enter the path/to/repository: '))
+        commit_message = (raw_input('Enter Commit message: '))
         file_list = input ("Array of path of files which you want to add: ")
         _commit_and_push_new_files(repo_dir, file_list, commit_message)
-        main_menu()
+        main()
 
     elif choice == 18:
-        path_to_repo = str(input('Enter the path/to/repository: '))
+        path_to_repo = (raw_input('Enter the path/to/repository: '))
         _git_pull(path_to_repo, remote_name='origin', branch='master')
-        main_menu()
+        main()
 
     elif choice == 19:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
         number = int(input('Enter the issue number: '))
         _get_issue(repo, number)
-        main_menu()
+        main()
 
     elif choice == 20:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
-        title = str(input('Enter the issue title: '))
+        title = (raw_input('Enter the issue title: '))
         _create_issue(repo, title)
-        main_menu()
+        main()
 
     elif choice == 21:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
-        title = str(input('Enter the issue title: '))
-        body =  str(input('Enter the issue: '))
+        title = (raw_input('Enter the issue title: '))
+        body =  (raw_input('Enter the issue: '))
         _create_issue_with_body(repo, title, body)
-        main_menu()
+        main()
 
     elif choice == 22:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
         _create_issue_with_labels(repo)
-        main_menu()
+        main()
 
     elif choice == 23:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
-        title = str(input('Enter the issue title: '))
-        github_username =  str(input('Enter your github username: '))
+        title = (raw_input('Enter the issue title: '))
+        github_username =  (raw_input('Enter your github username: '))
         _create_issue_with_body(repo, title, github_username)
-        main_menu()
+        main()
 
     elif choice == 24:
         g = _get_username_and_password("gshubh")
         repo = g.get_repo("gshubh/bucketlist")
         _create_issue_with_milestone(repo)
-        main_menu()
+        main()
 
     elif choice == 25:
         exit()
 
     else:
         print ("Invalid choice selection")
-        main_menu()
+        main()
 
 def main():
     main_menu()
